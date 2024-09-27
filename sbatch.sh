f65a28caff3cb2d37b1f0ef9b5213588e5b7ca1c
Yichao <yichao1liu@gmail.com>
2024-09-27 11:11:25 -0500

    feat: slurm script

---
+++ sbatch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=clm_pretrain    # 作业名称
+#SBATCH --nodes=4                  # 请求的节点数
+#SBATCH --ntasks-per-node=1         # 每个节点上的任务数
+#SBATCH --output=./logs/clm_pretrain_%j.out # 标准输出文件
+#SBATCH --error=./logs/clm_pretrain_%j.err  # 标准错误文件
+#SBATCH -p gpu-a100                 # 指定分区
+#SBATCH -t 48:00:00                 # 时间限制
+#SBATCH --mail-user=daizhilian@hotmail.com
+#SBATCH --mail-type=begin               
+#SBATCH --mail-type=end
+
+# 初始化 Conda 环境
+source /work/09735/yichao/ls6/miniconda/etc/profile.d/conda.sh
+
+# 激活你的 Conda 环境
+conda activate drugassist-jay
+
+# 切换到特定的工作目录
+cd /work/09735/yichao/ls6/zhilian/CLModel_v2_zl
+
+# export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
+
+# 获取主节点的地址
+MASTER_ADDR=$(hostname)
+
+# 执行PyTorch分布式训练命令
+# 端口最好使用随机的
+srun bash -c "torchrun --nproc_per_node=3 --nnodes=4 --node_rank=\$SLURM_NODEID --master_addr=$MASTER_ADDR --master_port=39688 train.py --model-choice transformer --num-epoch 200  --data-path /work/09735/yichao/ls6/zhilian/clm_code  --save-directory /work/09735/yichao/ls6/zhilian/clm_code/pretrain"
